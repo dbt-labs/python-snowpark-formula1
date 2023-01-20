22c0a67e0329d11bf0af141ea67498c0a3597162
Hope Watson <hope.watson@fishtownanalytics.com>
2023-01-20 02:42:31 +0000

    ensure all columns are uppercase for downstream testing

--- models/marts/aggregates/lap_times_moving_avg.py
+++ models/marts/aggregates/lap_times_moving_avg.py
@@ -12,5 +12,6 @@ def model(dbt, session):
     lap_time_trends = lap_times.groupby(by="RACE_YEAR")["LAP_TIME_SECONDS"].mean().to_frame()
     lap_time_trends.reset_index(inplace=True)
     lap_time_trends["LAP_MOVING_AVG_5_YEARS"] = lap_time_trends["LAP_TIME_SECONDS"].rolling(5).mean()
+    lap_time_trends.columns = lap_time_trends.columns.str.upper()
     
     return lap_time_trends.round(1)
