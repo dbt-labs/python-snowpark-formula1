f353c8655a28a5306bfc1017e3e5c2e21d358a58
Hope Watson <hope.watson@fishtownanalytics.com>
2023-05-01 21:52:29 +0000

    update variable names and comment out data recasting that is done upstream

--- models/marts/ml/prep/covariate_encoding.py
+++ models/marts/ml/prep/covariate_encoding.py
@@ -11,7 +11,7 @@ def model(dbt, session):
   data = dbt.ref("ml_data_prep").to_pandas()
 
   # list out covariates we want to use in addition to outcome variable we are modeling - position
-  covariates = data[['RACE_YEAR','RACE_NAME','GRID','CONSTRUCTOR_NAME','DRIVER','DRIVERS_AGE_YEARS','DRIVER_CONFIDENCE','CONSTRUCTOR_RELAIBLITY','TOTAL_PIT_STOPS_PER_RACE','ACTIVE_DRIVER','ACTIVE_CONSTRUCTOR', 'POSITION']]
+  covariates = data[['RACE_YEAR','RACE_NAME','GRID','CONSTRUCTOR_NAME','DRIVER','DRIVERS_AGE_YEARS','DRIVER_CONFIDENCE','CONSTRUCTOR_RELAIBLITY','TOTAL_PIT_STOPS_PER_RACE','ACTIVE_DRIVER','ACTIVE_CONSTRUCTOR', 'DRIVER_POSITION']]
  
   # filter covariates on active drivers and constructors
   # use fil_cov as short for "filtered_covariates"
@@ -37,7 +37,7 @@ def model(dbt, session):
 
   # we are dropping the columns that we filtered on in addition to our training variable
   encoded_data = fil_cov.drop(['ACTIVE_DRIVER','ACTIVE_CONSTRUCTOR'],1)
-  encoded_data['POSITION_LABEL']= encoded_data['POSITION'].apply(lambda x: position_index(x))
-  encoded_data_grouped_target = encoded_data.drop(['POSITION'],1)
+  encoded_data['POSITION_LABEL']= encoded_data['DRIVER_POSITION'].apply(lambda x: position_index(x))
+  encoded_data_grouped_target = encoded_data.drop(['DRIVER_POSITION'],1)
 
   return encoded_data_grouped_target
