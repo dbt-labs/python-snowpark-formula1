05d767ad0cb1788a39e57e967342fa2f17937f7b
Hope Watson <hope.watson@fishtownanalytics.com>
2023-05-09 18:15:48 +0000

    update fct_pit_stops column name and move aggregation up

--- models/core/fct_pit_stops.sql
+++ models/core/fct_pit_stops.sql
@@ -5,10 +5,11 @@ WITH pitstops AS (
         driver_id                                                                               AS driver_id,
         stop_number                                                                             AS stop_number,
         lap                                                                                     AS lap,
-        pit_stop_time                                                                           AS pitstop_time,
+        pit_stop_time                                                                           AS pit_stop_time,
         pit_stop_duration_seconds                                                               AS pit_stop_duration_seconds,
         pit_stop_duration                                                                       AS pit_stop_duration,
-        pit_stop_milliseconds                                                                   AS pit_stop_milliseconds
+        pit_stop_milliseconds                                                                   AS pit_stop_milliseconds,
+        max(stop_number) over (partition by race_id,driver_id) as total_pit_stops_per_race
     FROM {{ ref('stg_pit_stops') }}
 )
 
