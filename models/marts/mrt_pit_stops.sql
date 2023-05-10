8dcb52d0cff1117f62ef70a4cea67da2e056978a
Hope Watson <hope.watson@fishtownanalytics.com>
2023-05-10 14:26:16 +0000

    add column to mrt_pit_stops for total pit stops and update materialization for dbt_project.yml

--- models/marts/mrt_pit_stops.sql
+++ models/marts/mrt_pit_stops.sql
@@ -22,7 +22,8 @@ pit_stops_joined as (
         lap, 
         pit_stop_time,
         pit_stop_duration_seconds, 
-        pit_stop_milliseconds
+        pit_stop_milliseconds,
+        total_pit_stops_per_race
     from base_results
     left join pit_stops
         on base_results.race_id=pit_stops.race_id and base_results.driver_id=pit_stops.driver_id
